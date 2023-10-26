4cfcf8283143a4559f3f8b248b64e0ade977a690
Pankhuri Singh <119196051+Pankhuri0824@users.noreply.github.com>
2023-10-27 04:53:25 +0530

    Create Mammal.java

---
+++ Mammal.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Mammal extends Animal{
+
+    public Mammal(String type, String name, String sound, String info) {
+        super(type, name,sound, info);
+    }
+
+}
+
