f2e58208ffba05d7f31f5c982363f6ee9cff73c2
Pankhuri Singh <119196051+Pankhuri0824@users.noreply.github.com>
2023-10-27 04:53:03 +0530

    Create Discount.java

---
+++ Discount.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class Discount {
+    private String code; //name of discount code
+    private String category;
+    private int value;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Discount(String code, int value, String category) {
+        this.code = code;
+        this.value = value;
+        this.category=category;
+    }
+
+    @Override
+    public String toString() {
+        return "Discount code:" + code +
+                "\n Category:" + category +
+                "\nValue:" + value ;
+    }
+}
