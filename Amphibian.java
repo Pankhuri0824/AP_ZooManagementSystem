005564581d447a235f2ab9be8d3ae87328f220ef
Pankhuri Singh <119196051+Pankhuri0824@users.noreply.github.com>
2023-10-27 04:51:45 +0530

    Create Amphibian.java

---
+++ Amphibian.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Amphibian extends Animal{
+
+    public Amphibian(String type, String name, String sound, String info) {
+        super(type, name,sound, info);
+    }
+}
+
+
+//reptile a=new reptile
+//animal a=new reptile - Hashmap animal
