2c3d358ce626919eaa2b9bf91eb76fd811356988
Pankhuri Singh <119196051+Pankhuri0824@users.noreply.github.com>
2023-10-27 04:53:43 +0530

    Create Reptile.java

---
+++ Reptile.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Reptile extends Animal{
+    public Reptile(String type, String name, String sound, String info) {
+        super(type, name,sound, info);
+    }
+}
